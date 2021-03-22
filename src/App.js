28d9f81e4bd5ac4c8b847ab22220180d1a6fb923
Leonardo Tolosa <tolosa@gmail.com>
2021-03-22 17:57:05 -0300

    Improve example and add explanations

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { createPortal } from 'react-dom'
 
-const Portal = () => {
+const Alert = ({ children }) => {
   const content = (
-    <p>Portal content</p>
+    <div className="alert alert-danger text-uppercase" role="alert">
+      {children}
+    </div>
   )
 
-  return createPortal(content, document.body)
+  return createPortal(content, document.querySelector('.container'))
 }
 
 function App() {
   return (
-    <>
-      <p>hello!</p>
-      <Portal />
-    </>
+      <div className="jumbotron">
+        <h1 className="display-4">Portals sample</h1>
+        <hr />
+        <p className="lead">This is the main content component.</p>
+        <p className="mb-0">
+          Even though the <span className="text-monospace">Alert</span> component shown above is inside
+          this one, we use Portals to render it ouside, directly into the main Bootstrap container. Could be
+          useful for modals, toasts, etc.
+        </p>
+        <Alert>This is a fake alert!</Alert>
+      </div>
   )
 }
 
