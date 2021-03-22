00d936bb3ff01302ba758ac20266f6bfca612aac
Leonardo Tolosa <tolosa@gmail.com>
2021-03-22 18:24:39 -0300

    Improve code styling

--- src/App.js
+++ src/App.js
@@ -13,20 +13,18 @@ const Alert = ({ children }) => {
   return createPortal(content, container)
 }
 
-function App() {
-  return (
-      <div className="jumbotron">
-        <h1 className="display-4">Portals sample</h1>
-        <hr />
-        <p className="lead">This is the main content component.</p>
-        <p className="mb-0">
-          Even though the <span className="text-monospace">Alert</span> component shown above is inside
-          this one, we use Portals to render it ouside, directly into the main Bootstrap container. Could be
-          useful for modals, toasts, etc.
-        </p>
-        <Alert>This is a fake alert!</Alert>
-      </div>
-  )
-}
+const App = () => (
+  <div className="jumbotron">
+    <h1 className="display-4">Portals sample</h1>
+    <hr />
+    <p className="lead">This is the main content component.</p>
+    <p className="mb-0">
+      Even though the <span className="text-monospace">Alert</span> component shown above is inside
+      this one, we use Portals to render it ouside, directly into the main Bootstrap container. Could be
+      useful for modals, toasts, etc.
+    </p>
+    <Alert>This is a fake alert!</Alert>
+  </div>
+)
 
 export default App
