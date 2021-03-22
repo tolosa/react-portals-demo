bc4a3bcc086200023fea1b09e7c13dff26b7bdd8
Leonardo Tolosa <tolosa@gmail.com>
2021-03-22 17:27:05 -0300

    Add basic portal sample

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { createPortal } from 'react-dom'
+
+const Portal = () => {
+  const content = (
+    <p>Portal content</p>
+  )
+
+  return createPortal(content, document.body)
+}
+
 function App() {
   return (
     <>
-      <p>TODO: add page contents</p>
+      <p>hello!</p>
+      <Portal />
     </>
   )
 }
 
-export default App;
+export default App
